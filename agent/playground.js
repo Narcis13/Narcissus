8bd0082f3203f99eb3fb522735e03783c858092f
Narcis13 <Narcis75@gmail.com>
2025-05-08 00:02:24 +0300

    flows

--- agent/playground.js
+++ agent/playground.js
@@ -4,3 +4,29 @@
 /**
  * 
  */
+function State(initialState) {
+    return {
+        state: initialState,
+        setState(newState) {
+            this.state = newState;
+        },
+        getState() {
+            return this.state;
+        },
+        reset() {
+            this.state = initialState;
+        }                       
+    }
+}
+
+function Node(description='',adress='', func) {
+    
+   return func
+}
+
+function flow (nodes){
+    
+    return function(state){
+         return {}
+    }
+}
